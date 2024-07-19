e69debe20f246b8d6847bdbf2b2561d2986dd649
krishna <krishna.bogati@imark.com.np>
2024-07-19 16:48:09 +0545

    allow to choose any of the three available options

--- Program.cs
+++ Program.cs
@@ -4,13 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Compress using Normal Compression
-        //NormalCompression.Compress();
+        Console.Write("\n 1. Basic Compression \n 2. LZMA Algorithm \n 3. BZip2 Algorithm \n Choose the algorithm you want to use: ");
+        string selection = Console.ReadLine();
+        switch (selection)
+        {
+            case "1":
+                {
+                    //Compress using Normal Compression
+                    NormalCompression.Compress();
+                }
+                break;
+            case "2":
+                {
 
-        //Compress using LZMA Compression Algorithm.
-        //LZMACompression.Compress();
-
-        //Compress using Bzip2 Compression Algorithm
-        Bzip2Compression.Compress();
+                    //Compress using LZMA Compression Algorithm.
+                    LZMACompression.Compress();
+                }
+                break;
+            case "3":
+                {
+                    //Compress using Bzip2 Compression Algorithm
+                    Bzip2Compression.Compress();
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("Uh Oh! Your choice is beyond the provided Options. ");
+                }
+                break;
+        }
     }
 }
